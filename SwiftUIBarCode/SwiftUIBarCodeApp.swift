61760d24fa5b297773ef6c7671d1de57826484db
Kapil Goyal <kkgoyal@suyati.com>
2024-01-20 10:46:59 +0530

    Initial Commit

---
+++ SwiftUIBarCode/SwiftUIBarCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBarCodeApp.swift
+//  SwiftUIBarCode
+//
+//  Created by Kapil Kumar on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBarCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
