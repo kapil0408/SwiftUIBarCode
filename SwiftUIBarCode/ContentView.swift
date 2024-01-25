d71b80d1fc5ee0bcf453dde9927e7c0b2cffe7d9
Kapil Goyal <kkgoyal@suyati.com>
2024-01-25 10:24:09 +0530

    Add alert item

--- SwiftUIBarCode/ContentView.swift
+++ SwiftUIBarCode/ContentView.swift
@@ -7,21 +7,47 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable
+{
+    var id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input", message: "Something is wrong with the camera. we are unable to capture the input.", dismissButton: .default(Text("Ok")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scan Type", message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.", dismissButton: .default(Text("Ok")))
+
+}
+
 struct ContentView: View {
+    
+    @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
+    
     var body: some View {
         NavigationView {
             VStack {
                 //Rectangle().frame(maxWidth: .infinity, maxHeight: 300)
-                ScannerView().frame(maxWidth: .infinity, maxHeight: 300)
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
                 Spacer().frame(height: 60)
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder").font(.title)
-                Text("Not Yet Scanned")
+                Text(scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(scannedCode.isEmpty ? .red: .green)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
--- SwiftUIBarCode/ScannerView.swift
+++ SwiftUIBarCode/ScannerView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ScannerView: UIViewControllerRepresentable {
     
+    @Binding var scannedCode: String
+    @Binding var alertItem: AlertItem?
+    
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(scannerView: self)
     }
 
     typealias UIViewControllerType = ScannerVC
@@ -25,19 +28,31 @@ struct ScannerView: UIViewControllerRepresentable {
     
     final class Coordinator: NSObject, ScannerVCDelegate
     {
+        private let scannerView: ScannerView
+        
+        init(scannerView: ScannerView) {
+            self.scannerView = scannerView
+        }
+        
         func didFind(barcode: String) {
+            scannerView.scannedCode = barcode
             print(barcode)
         }
         
         func didSurface(error: CameraError) {
             print(error.self)
+            switch error {
+            case .invalidDeviceInput:
+                scannerView.alertItem = AlertContext.invalidDeviceInput
+            case .invalidScannedValue:
+                scannerView.alertItem = AlertContext.invalidScannedType
+            }
         }
-        
     }
 }
 
-struct ScannerView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScannerView()
-    }
-}
+//struct ScannerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ScannerView(scannedCode: .constant("123456"), alertItem: <#Binding<AlertItem?>#>)
+//    }
+//}
